5dea70e86b47ccd50b8f1fbbade157be21664263
h4haris <h.4haris@yahoo.com>
2023-02-22 10:39:02 +0500

    added sql problems

---
+++ SQL/003 Aggregation/011 Weather Observation Station 14.sql
@@ -0,0 +1,30 @@
+-- Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than 137.2345. Truncate your answer to 4 decimal places.
+
+
+
+--? Input Format
+
+
+-- The STATION table is described as follows:
+
+
+-- | Field  | Type         |
+-- | ------ | ------------ |
+-- | ID     | NUMBER       |
+-- | CITY   | VARCHAR2(21) |
+-- | STATE  | VARCHAR2(2)  |
+-- | LAT_N  | NUMBER       |
+-- | LONG_W | NUMBER       |
+
+
+-- where LAT_N is the northern latitude and LONG_W is the western longitude.
+
+
+
+
+
+Select Cast(Round(LAT_N,4,1) as numeric(36,4)) from Station
+where LAT_N = (select Max(LAT_N) from Station where LAT_N < 137.2345)
+
+
+-- 137.0193
